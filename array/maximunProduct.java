6b848aa377df3191fbd9fa9915b0f3e07ef3493c
Muskan Gautam <muskangautam7064@gmail.com>
2025-05-04 23:48:48 +0530

    maximum product of three

---
+++ array/maximunProduct.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.*;
+public class maximunProduct{
+    public static int maxProductOfTriplet(int arr[]){
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        return Math.max(arr[0]*arr[1]*arr[n-1] , arr[n-1]*arr[n-2]*arr[n-3]);
+    }
+    public static void main(String args[]){
+      int arr[] = {10,3,5,6,20};
+      System.out.println("maximum Product of tree is :"+maxProductOfTriplet(arr));
+    }
+ }
